ba09e1d1c630d70db657dcf8a5b3757b3d629944
DELL <1183723408@qq.com>
2021-03-14 10:35:40 +0800

    Example01

---
+++ src/views/Sidebar.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1>Vue实例</h1>
+    <ul>
+      <li><router-link to="/example01">Example01</router-link></li>
+    </ul>
+  </div>
+</template>
---
+++ src/views/example01/Example01.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h1>HelloWorld</h1>
+    <hr />
+    <h1>Computed</h1>
+    {{ message }}
+    <p>文本内容倒置</p>
+    {{ reverseMessage }}
+    <hr />
+    {{ user.name }}
+    <br />
+    <button type="button" @click="changeName">changeName</button>
+    <br />
+    {{ user.insertTime }}
+    <br />
+    {{ myDate }}
+    <br />
+    <button @click="addAddress">addAddress</button>
+    <br />
+    {{ user.address }}
+    <hr />
+    {{ myReplace(user.insertTime) }}
+    <hr />
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data: () => ({
+    isShow: false,
+    message: "hello",
+    user: {
+      name: "An",
+      insertTime: "2021-03-14T10:25:30",
+      address: ""
+    }
+  }),
+  methods: {
+    changeName() {
+      this.user.name = "Gao";
+    },
+    addAddress() {
+      this.user.address = "NEFU";
+    }
+  },
+  computed: {
+    reverseMessage(): string {
+      return this.message
+        .split("")
+        .reverse()
+        .join("");
+    },
+    myDate(): string {
+      return this.user.insertTime.replace("T", " ");
+    },
+    myReplace() {
+      return (date: string) => date.replace("T", " ");
+    }
+  }
+});
+</script>
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
